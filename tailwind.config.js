45fa95ed41cca9e52d7318efceb9bf802b33ac03
quas <65362920+quasdoto@users.noreply.github.com>
2025-09-24 22:34:10 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#b8dff4',
+      'gray-400': '#9ca3af',
+      'green-500': '#22c55e',
+      'primary-foreground': '#0c0c0e',
+      'secondary-foreground': '#e6e6eb',
+      'secondary': '#1e1e1f',
+      'muted-foreground': '#6c687d',
+      'card': '#0f0f10',
+      'card-foreground': '#1f2937',
+      'background': '#0d0c0d',
+      'gray-800': '#1f2937',
+    },
+    // b8dff4
+  },
+  plugins: [],
+  
+}
+
